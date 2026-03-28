78c5718bea0d80a14c21c4ce5e61f39637f72f34
washim04x <mdwashimmomin04x@gmail.com>
2026-03-28 18:55:26 +0530

    initial structure setup using cookiecutter and data added and verson controling using dvc

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A recommendation system that suggests similar movies when you like one, using cosine similarity on movie data.',
+    author='washim',
+    license='',
+)
